3be9f7060272e00d2d6cdc1ac42e5b4058a482ae
zjukd00m <zjukd00m@gmail.com>
2023-05-27 12:11:11 -0600

    Stylize the container of the contracts

--- app/src/components/Escrow/Escrow.js
+++ app/src/components/Escrow/Escrow.js
@@ -71,45 +71,47 @@ export default function Escrow({
     <div className="flex flex-col items-start justify-between m-2 bg-[#2E3440] border border-slate-600">
       <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center">
         <div className="m-3">
-          <div className="flex items-center justify-between mb-3">
-            <p className="text-grotesk text-sm md:text-md grow">
+          <div className="flex flex-col gap-3 items-center justify-between mb-3 md:flex">
+            <p className="text-grotesk text-sm md:text-md grow w-full">
               Contract address
             </p>
-            <div className="text-mono text-xs rounded-lg p-1 bg-[#6272a4] text-white w-fit">
-              {user?.wallet?.length && user.wallet === arbiter ? (
-                <p className="">Arbiter</p>
-              ) : user?.wallet?.length && user.wallet === beneficiary ? (
-                <p className="">Beneficiary</p>
-              ) : user?.wallet?.length ? (
-                <p className=""> Depositor </p>
+            <div className="flex items-center w-full">
+              <div className="text-mono text-xs rounded-lg p-1 bg-[#6272a4] text-white w-fit">
+                {user?.wallet?.length && user.wallet === arbiter ? (
+                  <p className="">Arbiter</p>
+                ) : user?.wallet?.length && user.wallet === beneficiary ? (
+                  <p className="">Beneficiary</p>
+                ) : user?.wallet?.length ? (
+                  <p className=""> Depositor </p>
+                ) : (
+                  <p className=""> Loading... </p>
+                )}
+              </div>
+              {isApproved ? (
+                <div className="relative ml-5">
+                  <div className="group flex gap-2 bg-[#6272a4] p-1 rounded-lg">
+                    <ion-icon
+                      name="checkmark-outline"
+                      style={{ color: '#50fa7b' }}
+                    ></ion-icon>
+                    <p className="text-xs text-mono"> Approved </p>
+                  </div>
+                </div>
               ) : (
-                <p className=""> Loading... </p>
+                <div className="relative ml-5">
+                  <div className="group flex gap-2 bg-[#6272a4] p-1 rounded-lg">
+                    <ion-icon
+                      name="alert-outline"
+                      style={{ color: '#ff5555' }}
+                    ></ion-icon>
+                    <p className="text-xs text-mono"> Waiting </p>
+                  </div>
+                </div>
               )}
             </div>
-            {isApproved ? (
-              <div className="relative ml-5">
-                <div className="group flex gap-2 bg-[#6272a4] p-1 rounded-lg">
-                  <ion-icon
-                    name="checkmark-outline"
-                    style={{ color: '#50fa7b' }}
-                  ></ion-icon>
-                  <p className="text-xs text-mono"> Approved </p>
-                </div>
-              </div>
-            ) : (
-              <div className="relative ml-5">
-                <div className="group flex gap-2 bg-[#6272a4] p-1 rounded-lg">
-                  <ion-icon
-                    name="alert-outline"
-                    style={{ color: '#ff5555' }}
-                  ></ion-icon>
-                  <p className="text-xs text-mono"> Waiting </p>
-                </div>
-              </div>
-            )}
           </div>
-          <div className="flex items-center gap-3 relative">
-            <p className="text-mono text-sm"> {address} </p>
+          <div className="flex items-center gap-3">
+            <p className="text-mono text-xs md:text-sm truncate"> {address} </p>
             {!copiedToClipboad ? (
               <ion-icon
                 name="clipboard-outline"
@@ -120,20 +122,12 @@ export default function Escrow({
             )}
           </div>
         </div>
-        <div className="flex flex-row-reverse md:flex-col md:items-end items-center">
-          <div
-            className="md:mr-3 ml-[10em]"
-            onClick={() => setExpanded(!expanded)}
-          >
-            <ion-icon name="chevron-down-outline"></ion-icon>
-          </div>
+        <div className="flex items-center justify-between pr-3 md:flex-col-reverse md:items-end w-full">
           {isApproved ? (
-            <div className="px-3 m-3 py-1 w-fit text-xs md:text-sm bg-green-100 text-black text-grotesk">
-              <p className="text-xs"> Approved! </p>
-            </div>
+            <div></div>
           ) : (
             <div
-              className={`px-3 py-1 bg-[#EBCB8B] text-black text-grotesk text-xs md:text-sm w-fit m-3 cursor-pointer ${
+              className={`px-3 py-1 bg-[#EBCB8B] text-black text-grotesk text-xs md:text-sm m-3 cursor-pointer ${
                 user?.wallet?.length && user.wallet !== arbiter
                   ? 'cursor-not-allowed'
                   : null
@@ -147,6 +141,12 @@ export default function Escrow({
               Approve
             </div>
           )}
+          <div
+            className="md:pr-3 md:pt-3"
+            onClick={() => setExpanded(!expanded)}
+          >
+            <ion-icon name="chevron-down-outline"></ion-icon>
+          </div>
         </div>
       </div>
       {expanded ? (
@@ -159,7 +159,7 @@ export default function Escrow({
               <div className="text-grotesk text-white uppercase text-sm mb-1">
                 Arbiter
               </div>
-              <div className="text-white text-mono text-xs md:text-sm">
+              <div className="text-white text-mono text-xs md:text-sm w-full truncate">
                 {arbiter}
               </div>
             </li>
@@ -167,7 +167,7 @@ export default function Escrow({
               <div className="text-grotesk text-white uppercase text-sm mb-1">
                 Beneficiary
               </div>
-              <div className="text-white text-mono md:text-sm text-xs">
+              <div className="text-white text-mono md:text-sm text-xs truncate">
                 {beneficiary}
               </div>
             </li>
